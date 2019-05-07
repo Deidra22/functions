c72f51afb0d0748594d3181268113b379aadfab7
Deidra22 <Deidra22@users.noreply.github.com>
2019-05-06 20:35:22 -0700

    learning about hoisting

---
+++ js/hoisting.js
@@ -0,0 +1,8 @@
+funcDeclaration();
+function funcDeclaration(){
+    console.log('I am inside a function declaration');
+}
+
+var funcExpression = (){
+    console.log('I am inside a function declaration');
+};
