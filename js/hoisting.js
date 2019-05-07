6797816d9670fb4ae8faa3d4a13340dae741b982
Deidra22 <Deidra22@users.noreply.github.com>
2019-05-06 20:35:41 -0700

    forgot to save

--- js/hoisting.js
+++ js/hoisting.js
@@ -1,8 +1,13 @@
+//function defined by declaration are hoisted to the top
 funcDeclaration();
 function funcDeclaration(){
     console.log('I am inside a function declaration');
 }
 
+//functions defined by an expression are not hoisted to the top. They remain where they are defined
+funcExpression();
 var funcExpression = (){
     console.log('I am inside a function declaration');
 };
+
+//so to avoid confusion, it is best practice to yse functions after they are defined
